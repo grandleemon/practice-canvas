2bc70f8bf515bce141fb167e7658e5a192b8d924
Max <zeetreex@gmail.com>
2022-10-14 14:43:18 +0300

    finished cursor animation

--- index.js
+++ index.js
@@ -13,16 +13,40 @@ window.addEventListener('load', () => {
             this.originX = Math.floor(x)
             this.originY = Math.floor(y)
             this.color = color
-            this.size = 5
-            this.vx = Math.random() * 2 - 1;
-            this.vy = Math.random() * 2 - 1;
+            this.size = this.effect.gap
+            this.vx = 0
+            this.vy = 0
+            this.ease = 0.03
+            this.friction = 0.95
+            this.dx = 0
+            this.dy = 0
+            this.distance = 0;
+            this.force = 0
+            this.angle = 0
         }
         draw(context){
+            context.fillStyle = this.color
             context.fillRect(this.x, this.y, this.size, this.size)
         }
         update(){
-            this.x += this.vx;
-            this.y += this.vy;
+            this.dx = this.effect.mouse.x - this.x
+            this.dy = this.effect.mouse.y - this.y
+            this.distance = this.dx * this.dx + this.dy * this.dy
+            this.force = -this.effect.mouse.radius / this.distance
+
+            if(this.distance < this.effect.mouse.radius){
+                this.angle = Math.atan2(this.dy, this.dx)
+                this.vx += this.force * Math.cos(this.angle)
+                this.vy += this.force * Math.sin(this.angle)
+            }
+
+            this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+            this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
+        }
+        warp(){
+            this.x = Math.random() * this.effect.width;
+            this.y = Math.random() * this.effect.height;
+            this.ease = 0.05
         }
     }
 
@@ -36,7 +60,16 @@ window.addEventListener('load', () => {
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
-            this.gap = 5;
+            this.gap = 8;
+            this.mouse = {
+                radius: 3000,
+                x: undefined,
+                y: undefined
+            }
+            window.addEventListener('mousemove', e => {
+                this.mouse.x = e.x
+                this.mouse.y = e.y
+            })
         }
         init(context) {
             context.drawImage(this.image, this.x, this.y)
@@ -62,12 +95,14 @@ window.addEventListener('load', () => {
         update(){
             this.particlesArray.forEach(particle => particle.update())
         }
+        warp(){
+            this.particlesArray.forEach(particle => particle.warp())
+        }
     }
 
     const effect = new Effect(canvas.width, canvas.height)
 
     effect.init(ctx)
-
     const animate = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         effect.update()
@@ -75,5 +110,10 @@ window.addEventListener('load', () => {
         requestAnimationFrame(animate)
     }
 
-    // animate()
+    animate()
+
+    const warpButton = document.getElementById('warpButton')
+    warpButton.addEventListener('click', () => {
+        effect.warp()
+    })
 })
