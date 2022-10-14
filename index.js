386f3a5313b7649440424df3e09280ad41b7c896
Max <zeetreex@gmail.com>
2022-10-14 12:59:36 +0300

    extracted colors and coordinates from raw pixel data using getImageData

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ window.addEventListener('load', () => {
     canvas.height = window.innerHeight
 
     class Particle {
-        constructor(effect) {
+        constructor(effect, x, y, color) {
             this.effect = effect
             this.x = Math.random() * this.effect.width
             this.y = Math.random() * this.effect.height
+            this.originX = Math.floor(x)
+            this.originY = Math.floor(y)
+            this.color = color
             this.size = 5
             this.vx = Math.random() * 2 - 1;
             this.vy = Math.random() * 2 - 1;
@@ -33,15 +36,28 @@ window.addEventListener('load', () => {
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
+            this.gap = 5;
         }
-        init() {
-            for(let i = 0; i < 100; i++) {
-                this.particlesArray.push(new Particle(this))
+        init(context) {
+            context.drawImage(this.image, this.x, this.y)
+            const pixels = context.getImageData(0, 0, this.width, this.height).data
+            for (let y = 0; y < this.height; y += this.gap) {
+                for(let x = 0; x < this.width; x += this.gap) {
+                    const index = (y * this.width + x) * 4;
+                    const red = pixels[index]
+                    const green = pixels[index + 1]
+                    const blue = pixels[index + 2]
+                    const alpha = pixels[index + 3]
+                    const color = `rgb(${red}, ${green}, ${blue})`
+
+                    if(alpha > 0){
+                        this.particlesArray.push(new Particle(this, x, y, color))
+                    }
+                }
             }
         }
         draw(context){
             this.particlesArray.forEach(particle => particle.draw(context))
-            context.drawImage(this.image, this.x, this.y)
         }
         update(){
             this.particlesArray.forEach(particle => particle.update())
@@ -50,7 +66,7 @@ window.addEventListener('load', () => {
 
     const effect = new Effect(canvas.width, canvas.height)
 
-    effect.init()
+    effect.init(ctx)
 
     const animate = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -59,5 +75,5 @@ window.addEventListener('load', () => {
         requestAnimationFrame(animate)
     }
 
-    animate()
+    // animate()
 })
